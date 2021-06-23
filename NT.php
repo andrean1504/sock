fac748c1bc45a3138a13eddc89c6c9670d1c2a01
andrean1504 <61500929+andrean1504@users.noreply.github.com>
2021-06-23 10:01:36 +0700

    Update NT.php

--- NT.php
+++ NT.php
@@ -5,4 +5,4 @@
 https://visionplus-stream.visionplus.id/live/eds/NetTV-M1/sa_hls/NetTV-M1-mp4a_66000_eng=3.m3u8
 #Video qualities
 #EXT-X-STREAM-INF:PROGRAM-ID=1,CLOSED-CAPTIONS=NONE,BANDWIDTH=625000,NAME=360p,RESOLUTION=640x360,AUDIO="aac"
-https://visionplus-stream.visionplus.id/live/eds/NetTV-M1/sa_hls/NetTV-M1-avc1_499968=4.m3u8
+https://visionplus-stream.visionplus.id/live/eds/NetTV-M1/sa_hls/NetTV-M1-avc1_499968=5.m3u8
